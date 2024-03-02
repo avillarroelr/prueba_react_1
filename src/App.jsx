8b41774bbb9027ec4cff7696e2bd85e2fa08e504
Andrés Villarroel Romero <127642205+avillarroelr@users.noreply.github.com>
2024-03-02 16:06:03 -0300

    firstCommit_proyectBaseComponentsAndLinks

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <button className="btn btn-primary">Mi Botón</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Buscador.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Buscador = () => {
+  return (
+    <div></div>
+  );
+}
+
+export default Buscador;
